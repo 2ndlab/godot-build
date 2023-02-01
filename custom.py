07749ea9497d30dec0faa294f8753cf6417be993
PeaceShi <peaceshi@outlook.com>
2023-02-02 04:28:01 +0800

    Add files via upload

---
+++ custom.py
@@ -0,0 +1,16 @@
+# Generated using https://godot-build-options-generator.github.io
+
+disable_3d = "yes"
+deprecated = "no"
+minizip = "no"
+module_arkit_enabled = "no"
+module_bullet_enabled = "no"
+module_camera_enabled = "no"
+module_hdr_enabled = "no"
+module_mobile_vr_enabled = "no"
+module_visual_script_enabled = "no"
+module_webxr_enabled = "no"
+
+debug_symbols = "no"
+production = "yes"
+openxr = "no"
